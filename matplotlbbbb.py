01379d2c8bfe22c5935a145f4284ce8f7189cda9
Md Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-06-20 20:27:11 +0600

    Update matplotlbbbb.py

--- matplotlbbbb.py
+++ matplotlbbbb.py
@@ -10,6 +10,19 @@ pt.scatter(data["rollno"],data["cgpa"],color="blue",label="scatter")
 pt.xlabel("RollNo",color="green") #xlabel() defines the label of x-axis
 pt.ylabel("CGPA",color="blue") #ylabel() defines the label of x-axis
 pt.title("CGPA vs Roll No",color="green") #title() is used to give title of this scatter plot
+#pt.show()
 pt.plot(data["rollno"],data["cgpa"],color="red",label="line graph") #plot() is used to create line graph
 pt.legend() # Calling&nbsp;legend()&nbsp;with no arguments automatically fetches the legend handlesand their associated labels
+pt.show()
+
+import matplotlib.pyplot as pt
+import pandas as pd
+data = pd.read_csv("cgpa.csv")
+data= data.head(30)
+#bar() method is used to plot a bar graph
+#Here i am taking a list of colors to showing graph attractive
+pt.bar(data["rollno"],data["cgpa"],color=["green","blue","pink","red"])
+pt.xlabel("RollNo",color="green")
+pt.ylabel("CGPA",color="blue")
+pt.title("CGPA vs Roll No",color="green")
 pt.show()
