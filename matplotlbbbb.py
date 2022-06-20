0da13ef4bd7581260e6d309ae726e69c26f78b41
Md Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-06-20 20:31:49 +0600

    Update matplotlbbbb.py

--- matplotlbbbb.py
+++ matplotlbbbb.py
@@ -48,3 +48,19 @@ pt.axis('equal') #for making pie chart circular,that makes major axis and minor 
 pt.pie([x,x1,x2],colors=['yellow','red','blue'],labels=['9 pointer','8 pointer','others'])
 pt.legend(title='Description') # to shown the labels as legends
 pt.show()
+
+#Histogram------
+
+
+import matplotlib.pyplot as pt
+import pandas as pd
+age_data = [20,30,54,66,70,10,45,65,77,99,120,130,29,40,80,75,90] #List of ages of peoples in aparticular place
+
+bins=[0,10,20,30,40,50,60,70,80,90,100,110,120] #The bins are usually specified as consecutive, non-overlapping intervals of a variable.
+
+#hist() is used to draw histogram
+pt.hist(age_data,bins,histtype='bar', rwidth=0.8)
+pt.title('Age distribution')
+pt.xlabel('Age')
+pt.ylabel('People')
+pt.show()
